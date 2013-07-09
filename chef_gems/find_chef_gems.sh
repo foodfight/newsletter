554cf643e2cfef53c173bf95b01426ecd4624e33
nathenharvey <nharvey@opscode.com>
2013-07-09 10:02:20 -0400

    Updates the script for finding chef gems

--- chef_gems/find_chef_gems.sh
+++ chef_gems/find_chef_gems.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-gem search -r '*chef' | grep -v knife- | grep -v bookchef | grep -v REMOTE | awk NF
+gem search -r 'chef' | grep -v knife- | grep -v bookchef | grep -v REMOTE | awk NF
 gem search -r berkshelf | grep -v REMOTE | awk NF
 gem search -r ridley | grep -v REMOTE | awk NF
 gem search -r foodcritic | grep -v REMOTE | awk NF
 gem search -r jvmargs | grep -v REMOTE | awk NF
 gem search -r vagrant-berkshelf | grep -v REMOTE | awk NF
 gem search -r vagrant-omnibus | grep -v REMOTE | awk NF
-gem search -r kitchen | grep -v kitchensink | grep -v REMOTE | awk NF
+gem search -r kitchen | grep -v kitchensink | grep -v gst-kitchen | grep -v chefkitchen_cli | grep -v REMOTE | awk NF
 gem search -r strainer | grep -v REMOTE | awk NF
 gem search -r busser | grep -v REMOTE | awk NF
 gem search -r emeril | grep -v REMOTE | awk NF
